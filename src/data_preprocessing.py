f378d6c554216dc90fa72a2aa2e6e5b3352e360b
Meshith Ariyawansa <71543277+Meshydra@users.noreply.github.com>
2023-05-10 21:05:49 +0530

    Dataset changes

--- src/data_preprocessing.py
+++ src/data_preprocessing.py
@@ -1,39 +1,60 @@
 import cv2
 import os
 
+
+def preprocess_images(images_dir, output_dir, target_size):
+    # Create the output directory if it doesn't exist
+    os.makedirs(output_dir, exist_ok=True)
+
+    # Get the list of image filenames in the directory
+    image_filenames = os.listdir(images_dir)
+
+    # Iterate over the image filenames
+    for filename in image_filenames:
+        # Construct the full path to the image
+        image_path = os.path.join(images_dir, filename)
+
+        # Load the image using OpenCV
+        image = cv2.imread(image_path)
+
+        # Resize the image
+        resized_image = cv2.resize(image, target_size)
+
+        # Normalize the pixel values to [0, 1]
+        normalized_image = resized_image / 255.0
+
+        # Apply additional preprocessing as needed
+        # For example, you can convert the image to grayscale, apply data augmentation, etc.
+
+        # Save the preprocessed image to the output directory
+        output_path = os.path.join(output_dir, filename)
+        cv2.imwrite(output_path, normalized_image * 255)
+
+
 # Path to the directory containing the vehicle images
+#test
 images_dir = './dataset/test'
-output_dir = './dataset/preprocessed'
+output_dir = './dataset/preprocessed/test'
 
-# Create the output directory if it doesn't exist
-os.makedirs(output_dir, exist_ok=True)
+target_size = (224, 224)
 
-# Get the list of image filenames in the directory
-image_filenames = os.listdir(images_dir)
+preprocess_images(images_dir, output_dir, target_size)
 
-# Define the target image size
-target_size = (224, 224)  # Example size, modify according to your requirements
+#val
+images_dir = './dataset/val'
+output_dir = './dataset/preprocessed/val'
 
-# Iterate over the image filenames
-for filename in image_filenames:
-    # Construct the full path to the image
-    image_path = os.path.join(images_dir, filename)
+target_size = (224, 224)
 
-    # Load the image using OpenCV
-    image = cv2.imread(image_path)
+preprocess_images(images_dir, output_dir, target_size)
+#train
+images_dir = './dataset/train'
+output_dir = './dataset/preprocessed/train'
 
-    # Resize the image
-    resized_image = cv2.resize(image, target_size)
+target_size = (224, 224)
 
-    # Normalize the pixel values to [0, 1]
-    normalized_image = resized_image / 255.0
+preprocess_images(images_dir, output_dir, target_size)
 
-    # Apply additional preprocessing as needed
-    # For example, you can convert the image to grayscale, apply data augmentation, etc.
-
-    # Save the preprocessed image to the output directory
-    output_path = os.path.join(output_dir, filename)
-    cv2.imwrite(output_path, normalized_image * 255)
 # Completion message
 print("Preprocessed")
 # Close any open windows
